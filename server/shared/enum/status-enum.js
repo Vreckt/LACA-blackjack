a10dda9ce3d0f747a29bcc509496b570ab999346
Logan <logan.carlier@gmail.com>
2020-08-13 19:01:16 +0200

    Reset server
    delete all files and all fodler

--- server/shared/enum/status-enum.js
+++
@@ -1,7 +0,0 @@
-const statusEnum = {
-    Error: 0,
-    Success: 1,
-}
-
-
-module.exports = statusEnum;
--- server/shared/enum/tableType.js
+++
@@ -1,7 +0,0 @@
-const tableType = {
-    Simple: 0,
-    Blackjack: 1,
-    Poker: 2,
-}
-
-module.exports = tableType;
