a8b245ca78957bf58d20bd293d40756a1fa506fe
Logan <logan.carlier@gmail.com>
2020-08-13 21:05:32 +0200

    init new server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const environnement = require('./src/environments/environnement');
+const package = require('./package');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+server.listen(environnement.port, () => {
+    console.log(`Version: ${package.version}, Server listening at port ${environnement.port}`);
+});
+
+io.on('init', (socket) => { });
